c747e0451096d5706efc4c467aa74e1a9becc47d
Gerhard Weis <g.weis@griffith.edu.au>
2018-01-24 14:10:59 +1000

    update to latest jupyter notebook ... find_kernel_specs not needed

--- environment_kernels/env_kernelspec.py
+++ environment_kernels/env_kernelspec.py
@@ -3,9 +3,9 @@
 from __future__ import absolute_import
 
 from jupyter_client.kernelspec import KernelSpec
-from traitlets import default
 
 _nothing = object()
+
 
 class EnvironmentLoadingKernelSpec(KernelSpec):
     """A KernelSpec which loads `env` by activating the virtual environment"""
@@ -19,7 +19,7 @@ class EnvironmentLoadingKernelSpec(KernelSpec):
             if self._loader:
                 try:
                     self._env = self._loader()
-                except:
+                except Exception:
                     self._env = {}
         return self._env
 
@@ -27,14 +27,13 @@ class EnvironmentLoadingKernelSpec(KernelSpec):
         self._loader = loader
         super(EnvironmentLoadingKernelSpec, self).__init__(**kwargs)
 
-
     def to_dict(self):
-        d = dict(argv=self.argv,
-                 # Do not trigger the loading
-                 #env=self.env,
-                 display_name=self.display_name,
-                 language=self.language,
-                )
+        d = dict(
+            argv=self.argv,
+            # Do not trigger loading
+            # env=self.env,
+            display_name=self.display_name,
+            language=self.language,
+        )
 
         return d
-
